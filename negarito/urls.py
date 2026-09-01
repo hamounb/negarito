8d21a5a9da8ec970b67c1a62c9e4dd5d8f39703d
Hamoun Bamoradi <hamounbamoradi@gmail.com>
2026-09-01 17:33:27 +0330

    test v1.0.0 blog

--- negarito/urls.py
+++ negarito/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
